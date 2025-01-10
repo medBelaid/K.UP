02b26b9dcdad8ba2c85fcae988ee726f4cac5f6b
medBelaid <medbbelaid@gmail.com>
2025-01-10 21:18:09 +0100

    quesion3: check data before adding user

--- src/user/user.types.ts
+++ src/user/user.types.ts
@@ -35,6 +35,7 @@ export class User implements IUser {
 @ArgsType()
 export class AddUser implements IAddUser {
   @MaxLength(50)
+  @IsNotEmpty({ message: `Le nom de l'utilisateur n'est pas défini` })
   @Field(() => String)
   name: string;
 
@@ -42,6 +43,9 @@ export class AddUser implements IAddUser {
   emails?: UserEmail[];
 
   @IsOptional()
+  @MaxDate(new Date(), {
+    message: 'La date de naissance ne peut pas être définie dans le future',
+  })
   @Field(() => Date, { nullable: true })
   birthdate?: Date;
 }
